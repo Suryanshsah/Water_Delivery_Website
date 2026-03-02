082d0c6b80a1afb59c442b821e7b47fe9863d69b
Suryansh <suryanshshankar20@gmail.com>
2026-03-02 10:05:19 +0530

    adding dark mode

---
+++ script.js
@@ -0,0 +1,18 @@
+const toggle = document.getElementById("darkToggle");
+
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+  toggle.checked = true;
+}
+
+
+toggle.addEventListener("change", () => {
+  if (toggle.checked) {
+    document.body.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.body.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  }
+});
